c8449ee2bdc35b5a20f7a358fd32616c3aa8b156
Siddharth-singh2111 <siddharth.s23@iiits.in>
2026-04-03 10:10:57 +0530

    change in color coding and some minor design changes

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -59,11 +59,11 @@ export default function TransactionList() {
         <div className="flex flex-col sm:flex-row gap-3">
           {/* Search Bar */}
           <div className="relative">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" size={16} />
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-red-400" size={16} />
             <input
               type="text"
               placeholder="Search description..."
-              className="pl-9 pr-4 py-2 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="pl-9 pr-4 py-2 border border-black-300 rounded-lg text-sm text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
@@ -71,7 +71,7 @@ export default function TransactionList() {
 
           {/* Category Filter */}
           <select
-            className="px-3 py-2 border border-gray-300 rounded-lg text-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="px-3 py-2 border border-gray-300 rounded-lg text-black text-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
             value={categoryFilter}
             onChange={(e) => setCategoryFilter(e.target.value)}
           >
